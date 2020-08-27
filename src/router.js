96a62face552af78839b7bf0dd0ceb16647624ae
Gabriel santos teles <69699850+gabrielprog@users.noreply.github.com>
2020-08-27 02:33:46 -0300

    Add middleware in router

--- src/router.js
+++ src/router.js
@@ -2,11 +2,14 @@ import Router from 'express';
 import UserController from './app/controller/userController';
 import SessionController from './app/controller/sessionController';
 import RecipientController from './app/controller/recipientController';
+import authMiddleware from './app/middleware/authMiddleware';
 
 const routers = Router();
 
 routers.post("/register",UserController.store);
 routers.post("/session", SessionController.store);
+
+routers.use(authMiddleware);
 routers.get("/registered/updated", RecipientController.store);
 
 export default routers;
