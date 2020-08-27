daf9a1a8ce500596fd041213f6fafe6183430219
Gabriel santos teles <69699850+gabrielprog@users.noreply.github.com>
2020-08-27 00:22:32 -0300

    Add router register

--- src/router.js
+++ src/router.js
@@ -1,9 +1,8 @@
 import Router from 'express';
+import UserController from './app/controller/userController';
 
 const routers = Router();
 
-routers.get("/",() =>{
-    console.log("Yes");
-});
+routers.post("/register",UserController.store);
 
 export default routers;
