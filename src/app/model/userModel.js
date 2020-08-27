def31ff1aa20aba2c65f4cc9bc27703b051f00a5
Gabriel santos teles <69699850+gabrielprog@users.noreply.github.com>
2020-08-27 00:26:34 -0300

    Add Cryptography Model User

--- src/app/model/userModel.js
+++ src/app/model/userModel.js
@@ -1,16 +1,23 @@
-import {DataType, Model} from 'sequelize';
+import  Sequelize, {Model} from 'sequelize';
+import cryptography from 'bcrypt';
 
 class User extends Model {
     static init(sequelize) {
-        super.init('user',{
-            name: DataType.STRING,
-            email: DataType.STRING,
-            password: DataType.STRING
+        super.init({
+            name: Sequelize.STRING,
+            email: Sequelize.STRING,
+            password: Sequelize.STRING
         },
         {
             sequelize
         });
+
+        this.addHook("beforeSave", async (password) => {
+            if(password.password){
+                password.password = await cryptography.hash(password.password, 8);
+            }
+        });
     }
 }
 
-export default User.init;
+export default User;
