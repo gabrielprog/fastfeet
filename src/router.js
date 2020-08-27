981bfdf64340e69f64d67053a81d6ba4a3cb73fb
Gabriel santos teles <69699850+gabrielprog@users.noreply.github.com>
2020-08-27 01:06:58 -0300

    Add router session

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import Router from 'express';
 import UserController from './app/controller/userController';
+import SessionController from './app/controller/sessionController';
 
 const routers = Router();
 
 routers.post("/register",UserController.store);
+routers.post("/session", SessionController.store);
 
 export default routers;
