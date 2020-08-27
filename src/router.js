07da2bae41fee1e93ff73880df37002566e76ff1
Gabriel santos teles <69699850+gabrielprog@users.noreply.github.com>
2020-08-27 02:27:29 -0300

    Change post to get router recipient

--- src/router.js
+++ src/router.js
@@ -7,6 +7,6 @@ const routers = Router();
 
 routers.post("/register",UserController.store);
 routers.post("/session", SessionController.store);
-routers.post("/cadastrados/atualizados", RecipientController.store);
+routers.get("/registered/updated", RecipientController.store);
 
 export default routers;
