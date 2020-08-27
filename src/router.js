11c229c0e3cac2b83082196a8bbdc198112724ea
Gabriel santos teles <69699850+gabrielprog@users.noreply.github.com>
2020-08-27 02:22:14 -0300

    Add router recipient

--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import Router from 'express';
 import UserController from './app/controller/userController';
 import SessionController from './app/controller/sessionController';
+import RecipientController from './app/controller/recipientController';
 
 const routers = Router();
 
 routers.post("/register",UserController.store);
 routers.post("/session", SessionController.store);
+routers.post("/cadastrados/atualizados", RecipientController.store);
 
 export default routers;
