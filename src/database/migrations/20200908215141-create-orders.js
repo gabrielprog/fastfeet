71e2459a21c44a97c9ce3ba8959bfcba8ff356e6
Gabriel santos teles <69699850+gabrielprog@users.noreply.github.com>
2020-09-08 19:04:49 -0300

    Create migration orders

---
+++ src/database/migrations/20200908215141-create-orders.js
@@ -0,0 +1,73 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('orders', { 
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      recipient_id: {
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'recipients',
+          key: 'id'
+        },
+       onUpdate: 'CASCADE',
+       onDelete: 'SET NULL',
+       allowNull: true
+      },
+      deliveryman_id: {
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'deliverymans',
+          key: 'id'
+        },
+       onUpdate: 'CASCADE',
+       onDelete: 'SET NULL',
+       allowNull: true
+      },
+      signature_id: {
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'signatures',
+          key: 'id'
+        },
+       onUpdate: 'CASCADE',
+       onDelete: 'SET NULL',
+       allowNull: true
+      },
+      product: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      canceled_at: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      start_date: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      end_date: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      } 
+    });
+    
+  },
+
+  down: async queryInterface => {
+    await queryInterface.dropTable('orders');
+  }
+};
