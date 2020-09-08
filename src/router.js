ba2574bf850aee1aee2c9b70195d7013f45da4bf
Gabriel santos teles <69699850+gabrielprog@users.noreply.github.com>
2020-09-08 01:03:44 -0300

    Add upload file

--- src/router.js
+++ src/router.js
@@ -1,12 +1,16 @@
 import Router from 'express';
+import multer from "multer";
 import UserController from './app/controller/userController';
 import SessionController from './app/controller/sessionController';
 import RecipientController from './app/controller/recipientController';
 import DeliverymanController from './app/controller/deliverymanController';
+import AvatarController from './app/controller/avatarController';
 import authMiddleware from './app/middleware/authMiddleware';
 import checkAdminMiddleware from './app/middleware/checkAdminMiddleware';
+import configMulter from './config/configMulter';
 
 const routers = Router();
+const upload = multer(configMulter);
 // ADMIN RESPONSIBILITY
 routers.post("/register",UserController.store);
 routers.post("/session", SessionController.store);
@@ -18,6 +22,7 @@ routers.get('/admin/deliverymans', checkAdminMiddleware, DeliverymanController.i
 routers.put('/admin/deliverymans', checkAdminMiddleware, DeliverymanController.update);
 routers.delete('/admin/deliverymans/:id', checkAdminMiddleware, DeliverymanController.delete);
 routers.post('/admin/deliverymans', checkAdminMiddleware, DeliverymanController.store);
+routers.post('/admin/deliverymans/upload', checkAdminMiddleware, upload.single('file'), AvatarController.store);
 
 // RECIPIENT RESPONSIBILITY
 routers.post("/registered/updated", RecipientController.store);
