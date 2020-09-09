c204285001274d1096954f363b39cf0481136373
Gabriel santos teles <69699850+gabrielprog@users.noreply.github.com>
2020-09-09 02:58:18 -0300

    Add template email config

--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -1,5 +1,8 @@
 import nodemailer from 'nodemailer';
 import configMail from '../config/configMail';
+import expresshbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
+import {resolve} from 'path';
 
 class Mail {
     constructor(){
@@ -10,6 +13,23 @@ class Mail {
             port,
             auth: auth.user ? auth : null
         });
+
+        this.configTamplatesEmail();
+    }
+
+    configTamplatesEmail() {
+        const pathEmail = resolve(__dirname, '..', 'app', 'view', 'email');
+
+        this.transporter.use('compile', nodemailerhbs({
+            viewEngine: expresshbs.create({
+                layoutsDir: resolve(pathEmail, 'layouts'),
+                partialsDir: resolve(pathEmail, 'partials'),
+                defaultLayout: 'default',
+                extname: '.hbs'
+            }),
+            viewPath: pathEmail,
+            extName: '.hbs'
+        }));
     }
 
     sendMail(message) {
