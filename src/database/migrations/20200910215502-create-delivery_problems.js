56618ff8a0994c333982ad8ea7b4dbe3d15fbc6b
Gabriel santos teles <69699850+gabrielprog@users.noreply.github.com>
2020-09-10 19:00:49 -0300

    Create migration delivery_problems

---
+++ src/database/migrations/20200910215502-create-delivery_problems.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('delivery_problems', { 
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      delivery_id: {
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'deliverymans',
+          key: 'id'
+        },
+       onUpdate: 'CASCADE',
+       onDelete: 'SET NULL',
+       allowNull: true
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      } 
+    });
+    
+  },
+
+  down: async queryInterface => {
+    await queryInterface.dropTable('delivery_problems');
+  }
+};
