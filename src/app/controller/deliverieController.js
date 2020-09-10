27333fe4525a35d81bfbe1bdeb5b90c7fdce7890
Gabriel santos teles <69699850+gabrielprog@users.noreply.github.com>
2020-09-09 23:31:02 -0300

    Add orderCanceled, orderSent

--- src/app/controller/deliverieController.js
+++ src/app/controller/deliverieController.js
@@ -1,4 +1,5 @@
 import Order from '../model/orderModel';
+import {Op} from 'sequelize';
 
 class DeliverieController {
     async index(request, response){
@@ -12,7 +13,31 @@ class DeliverieController {
             }
         });
 
-        return response.status(200).json(orderForSend);
+        const orderSent = await Order.findAll({
+            where: {
+                deliveryman_id: id,
+                canceled_at: null,
+                end_date: {
+                    [Op.not]: null
+                }
+            }
+        });
+
+        
+        const orderCanceled = await Order.findAll({
+            where: {
+                deliveryman_id: id,
+                canceled_at: {
+                    [Op.not]: null
+                }
+            }
+        });
+
+        return response.status(200).json({
+            orderForSend: orderForSend,
+            orderSent: orderSent,
+            orderCanceled: orderCanceled
+        });
     }
 }
 
