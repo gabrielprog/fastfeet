6ed52fc6122c0c744ad7ff8a7d53b45a09cafa17
Gabriel santos teles <69699850+gabrielprog@users.noreply.github.com>
2020-09-11 19:05:06 -0300

    Update name table

--- src/database/migrations/20200910215502-create-delivery_problems.js
+++ src/database/migrations/20200910215502-create-delivery_problems.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('delivery_problems', { 
+    await queryInterface.createTable('deliveryproblems', { 
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
