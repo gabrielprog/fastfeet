7b3d6e4ad899f4d42e80fa577bbf85e38c9b852a
Gabriel santos teles <69699850+gabrielprog@users.noreply.github.com>
2020-09-11 22:33:29 -0300

    Create delete

--- src/app/controller/deliveryproblemController.js
+++ src/app/controller/deliveryproblemController.js
@@ -1,4 +1,8 @@
 import Deliveryproblem from '../model/deliveryproblemModel';
+import Order from '../model/orderModel';
+import Deliveryman from '../model/deliverymanModel';
+import Recipient from '../model/recipientsModel';
+import Mail from '../../lib/Mail';
 import * as yup from 'yup';
 
 class DeliveryproblemController {
@@ -29,6 +33,47 @@ class DeliveryproblemController {
         });
 
         return response.status(201).json(putProblem);
+    }
+
+    async delete(request, response){
+        const {id} = request.params;
+        const fetchOrder = await Deliveryproblem.findByPk(
+            id,
+            {
+                include: [{
+                    model: Order,
+                    as: 'order',
+                    include: [{
+                        model: Deliveryman,
+                        as: 'deliveryman'
+                    },
+                    {
+                        model: Recipient,
+                        as: 'recipient'
+                    }
+                ]
+                }]
+            });
+            
+        fetchOrder.order.canceled_at = new Date();
+
+        await Mail.sendMail({
+            to: `${fetchOrder.order.deliveryman.name} <${fetchOrder.order.deliveryman.email}>`,
+            subject: 'Encomenda cancelada',
+            template: 'cancel',
+            context: {
+                name: fetchOrder.order.deliveryman.name,
+                nameRecipient: fetchOrder.order.recipient.name,
+                street: fetchOrder.order.recipient.street,
+                number: fetchOrder.order.recipient.number,
+                cep: fetchOrder.order.recipient.postal_code,
+                product: fetchOrder.order.product,
+             }
+        });
+
+        fetchOrder.order.save();
+
+        return response.status(200).json({status: true});
     }
 }
 
