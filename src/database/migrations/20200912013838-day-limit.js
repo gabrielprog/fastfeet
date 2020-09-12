9b13cc90986d8db7881749df3b34fb622f6f151c
Gabriel santos teles <69699850+gabrielprog@users.noreply.github.com>
2020-09-11 22:43:51 -0300

    Create migration daylimits

---
+++ src/database/migrations/20200912013838-day-limit.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('daylimits', { 
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      deliveryman_id: {
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'deliverymans',
+          key: 'id'
+        },
+       onUpdate: 'CASCADE',
+       onDelete: 'SET NULL',
+       allowNull: true
+      },
+      count_used: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      } 
+    });
+    
+  },
+
+  down: async queryInterface => {
+    await queryInterface.dropTable('daylimits');
+  }
+};
