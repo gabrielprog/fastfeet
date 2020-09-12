0685691fd44ea5c7331caa9d32ebf3530039c102
Gabriel santos teles <69699850+gabrielprog@users.noreply.github.com>
2020-09-12 00:57:26 -0300

    Update signature config

--- src/config/configMulter.js
+++ src/config/configMulter.js
@@ -30,7 +30,29 @@ export default {
     },
 
     signature:{
-        
+        storage: multer.diskStorage({
+            destination: resolve(__dirname, '..', '..', 'tmp', 'uploads', 'signature'),
+            filename: (request, file, callback) => {
+    
+                const filetypes = /jpeg|jpg|png|gif/;
+                const ename = filetypes
+                .test(extname(file.originalname).toLowerCase());
+                const mimetype = filetypes.test(file.mimetype);
+    
+                if(!mimetype && !ename){
+                    return callback('Error: Images Only!');
+                }
+    
+                crypto.randomBytes(16, (err, response) => {
+                    if(err){ 
+                        return callback(err);
+                    }
+                    
+                    return callback(null, 
+                        response.toString('hex') + extname(file.originalname));
+                });
+            }
+        })
     }
     
 }
